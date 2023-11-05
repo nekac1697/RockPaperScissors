8d8c350d687ed8e196fa7f8bbb242ebcdcee2905
Nemanja <nemanjavukojevic44@gmail.com>
2023-11-05 17:29:22 -0500

    Added user choice using prompts Created game logic Made small GUI to display winner

--- index.js
+++ index.js
@@ -2,12 +2,64 @@ function computerMove() {
   let choice;
   let randomNumber = Math.round(Math.random() * 2);
   if (randomNumber == 0) {
-    choice = "Rock";
+    choice = "ROCK";
   } else if (randomNumber == 1) {
-    choice = "Paper";
+    choice = "PAPER";
   } else {
-    choice = "Scissors";
+    choice = "SCISSORS";
   }
   return choice;
 }
-console.log(computerMove());
+
+function userMove() {
+  let choice = prompt("Please enter your choice");
+  choice = choice.toUpperCase();
+  console.log(choice);
+  if (choice !== "ROCK" && choice !== "PAPER" && choice !== "SCISSORS") {
+    alert("Wrong input. You inputed " + choice + ". Try Again.");
+    choice = prompt("Please enter your choice");
+    choice = choice.toUpperCase();
+  } else {
+    return choice;
+  }
+}
+
+function calculateWinner(cpuChoice, userChoice) {
+  if (cpuChoice == userChoice) {
+    return "Draw. No winner";
+  } else if (cpuChoice == "ROCK" && userChoice == "PAPER") {
+    console.log("User wins!");
+    return "User wins!";
+  } else if (cpuChoice == "PAPER" && userChoice == "SCISSORS") {
+    console.log("User wins!");
+    return "User wins!";
+  } else if (cpuChoice == "SCISSORS" && userChoice == "ROCK") {
+    console.log("User wins!");
+    return "User wins!";
+  } else {
+    console.log("CPU Wins" + cpuChoice + userChoice);
+    return "CPU wins!";
+  }
+}
+
+function playRound() {
+  let cpuChoice = computerMove();
+  let userChoice = userMove();
+  let winner = calculateWinner(cpuChoice, userChoice);
+  let score = document.getElementById("score");
+  let cpuScore = document.getElementById("cpu");
+  let userScore = document.getElementById("user");
+  if (winner == "Draw. No winner") {
+    return;
+  } else if (winner == "User wins!") {
+    userPoints++;
+    userScore.textContent = "User:" + userPoints;
+  } else {
+    cpuPoints++;
+    cpuScore.textContent = "Computer:" + cpuPoints;
+  }
+  score.textContent = winner;
+}
+
+let userPoints = 0;
+let cpuPoints = 0;
